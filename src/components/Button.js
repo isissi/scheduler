7a4260074bf687f5dca1f0540dedf7aabc7f771d
isissi <isissi_525@hotmail.com>
2022-02-21 10:36:55 -0700

    add button interactivity

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = 'button';
+
+   if (props.confirm) {
+      buttonClass += ' button--confirm';
+   }
+
+   if (props.danger) {
+      buttonClass += ' button--danger';
+   }
+
+   return (
+      <button disabled={props.disabled} className={buttonClass} onClick={props.onClick}>
+         {props.children}
+      </button>
+   );
 }
