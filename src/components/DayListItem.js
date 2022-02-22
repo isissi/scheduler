ade7d55c60561e3232a9d31de844f3acde67c6f2
isissi <isissi_525@hotmail.com>
2022-02-21 21:46:02 -0700

    fix .map function error

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -19,7 +19,7 @@ export default function DayListItem(props) {
   const spotMessage = formatSpots(props.spots);
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className={"text--regular"}>{props.name}</h2>
       <h3 className="text--light">{spotMessage}</h3>
     </li>
