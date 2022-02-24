74f70d6bf230f4ca99a3cb692459e8876cdf8408
isissi <isissi_525@hotmail.com>
2022-02-24 11:45:34 -0700

    add Empty, Show, Confirm stories

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,9 @@ import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
+import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
 
 storiesOf("Button", module)
   .addParameters({
@@ -142,4 +145,7 @@ storiesOf('Appointment', module)
 })
 .add('Appointment', () => <Appointment />)
 .add('Appointment with Time', () => <Appointment time='12pm'/>)
-.add('Header', () => <Header time='12pm'/>);
+.add('Header', () => <Header time='12pm'/>)
+.add('Empty', () => <Empty onAdd={action('onAdd')} />)
+.add('Show', () => <Show student='Lydia Miller-Jones' interviewer={interviewer} onEdit={action('onEdit')} onDelete={action('onDelete')}  />)
+.add('Confirm', () => <Confirm message='Delete the appointment?' onConfirm={action('onConfirm')} onCancel={action('onCancel')} />)
