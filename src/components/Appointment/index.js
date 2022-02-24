b5b72c2f4120d55de7eda8331e079ddccb78d36a
isissi <isissi_525@hotmail.com>
2022-02-24 11:46:02 -0700

    add style

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import 'components/Appointment/styles.scss'
 
 export default function Appointment (props) {
 
   return (
     <article className="appointment">
-      {!props.time ? 'No Appointments' : `Appointment at ${props.time}`}
+      {!props.time ? 'No Appointments' : `Appointment at ${props.time }`}
     </article>
   )
 }
