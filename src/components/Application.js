5038f6b09514c459f8b2b2736288f0cd34b61507
isissi <isissi_525@hotmail.com>
2022-03-01 20:51:07 -0700

    add interviews data and getInterview

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,65 +4,38 @@ import DayListItem from "./DayListItem";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from 'axios';
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: 'Monday', 
-    days: []
-    // appointments: {}
+    days: [],
+    appointments: {}, 
+    interviewers: {}
   })
 
   const setDay = day => setState(prev => ({...prev, day }));
-  const setDays = days => setState(prev => ({...prev, days }));
 
   useEffect(() => {
-    axios.get('/api/days').then((res) => {setDays(res.data);})
+    Promise.all([
+      axios.get('/api/days'), 
+      axios.get('/api/appointments'), 
+      axios.get('/api/interviewers')
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+    })
   }, [])
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
  
-  const appointmentList = Object.values(appointments).map((appointment) => {
+  const appointmentList = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
     return (
     <Appointment 
       key={appointment.id} 
       {...appointment}
+      interview={interview}
     />
     )
   })
