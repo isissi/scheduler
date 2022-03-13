9311e19e221c218873e5b3235f08ff66ef85c173
isissi <isissi_525@hotmail.com>
2022-03-12 20:32:57 -0700

    fix edit save error back to empty instead of edit

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,20 +5,21 @@ export default function useVisualMode (initial) {
   const [history, setHistory] = useState([initial]);
 
   const transition = (addMode, replace = false) => {
-    setMode(addMode);
     if (!replace) {
       setHistory(prev => [...prev, addMode]);
     } else {
-      //Change history to a copy of the history with newMode at the end
-      setHistory(prev => prev.slice(0, history.length - 1))
+    //Change history to a copy of the history with newMode at the end
+      setHistory(prev => prev.slice(0, -1));
+      console.log(history);
       setHistory(prev => [...prev, addMode]);
     }
+    setMode(addMode);
   };
 
   const back = () => { 
-    //Checks if history has atleast two items
+    //Checks if history has at least two items
     if (history.length > 1) {
-      setHistory(history.slice(0, history.length - 1)); 
+      setHistory(history.slice(0, -1));
       setMode(history[history.length - 2]);
     }
   };
