1f7e7b1a03f7bf322173ba8fa63090b9aec69412
isissi <isissi_525@hotmail.com>
2022-03-18 14:16:38 -0600

    add describe & test loads data

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,17 +1,30 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it('changes the schedule when a new day is selected', async () => {
-  const { getByText } = render(<Application />);
 
-  await waitForElement(() => getByText('Monday'));
 
-  fireEvent.click(getByText('Tuesday'));
+describe('Application', () => {
+  it('changes the schedule when a new day is selected', async () => {
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText('Monday'));
+  
+    fireEvent.click(getByText('Tuesday'));
+  
+    expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  });
 
-  expect(getByText('Leopold Silvers')).toBeInTheDocument();
+  it('loads data, books an interview and reduces the spots remaining for the first day by 1', async () => {
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+    
+    console.log(prettyDOM(container));
+  });
 })
+
