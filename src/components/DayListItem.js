74a5867716676d5f58d9096242cd74a58758ee97
isissi <isissi_525@hotmail.com>
2022-03-18 18:29:31 -0600

    add testid to <li>

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -19,7 +19,7 @@ export default function DayListItem(props) {
   const spotMessage = formatSpots(props.spots);
 
   return (
-    <li className={dayClass} onClick={props.setDay}>
+    <li className={dayClass} onClick={props.setDay} data-testid='day'>
       <h2 className={"text--regular"}>{props.name}</h2>
       <h3 className="text--light">{spotMessage}</h3>
     </li>
